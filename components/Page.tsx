a1e4f07524042fe5d92f4b411010f0505e625e97
Andrew Lisowski <lisowski54@gmail.com>
2021-05-09 15:42:07 -0700

    add follow links

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,5 +1,40 @@
 import { AutoThemeProvider } from "@devtools-ds/themes";
+import makeClass from "clsx";
 import Head from "next/head";
+
+const PODCAST_LINKS: ActionLinkProps[] = [
+  {
+    src: "/apple.png",
+    href: "https://podcasts.apple.com/podcast/id1566647758",
+    text: "Apple Podcasts",
+  },
+  {
+    src: "/spotify.png",
+    href: "https://open.spotify.com/show/142I2b9HGjWhRgPXhOYUnN",
+    text: "Spotify",
+  },
+  {
+    src: "/youtube.png",
+    href: "https://www.youtube.com/channel/UCFsRlOn7gODgv6WUriLrzXg",
+    text: "YouTube",
+  },
+  {
+    src: "/twitter.png",
+    href: "https://twitter.com/devtoolsfm",
+    text: "Twitter",
+  },
+  {
+    src: "/rss.png",
+    href: "https://feeds.buzzsprout.com/1772992.rss",
+    text: "RSS Feed",
+  },
+];
+
+interface ActionLinkProps {
+  src: string;
+  href: string;
+  text: string;
+}
 
 const FooterLink = (
   props: Omit<React.ComponentProps<"a">, "target" | "rel" | "className">
@@ -12,22 +47,36 @@ export const Footer = () => {
 
   return (
     <footer className="text-center mb-12 md:mb-16 mt-8">
-      <p className="mb-6">
+      <p>
         Copyright © {year}{" "}
         <FooterLink href="https://twitter.com/hipstersmoothie">
           Andrew Lisowski
         </FooterLink>
-        .{" "}
       </p>
 
-      <p className="flex justify-center mb-6">
-        <FooterLink href="https://vercel.com?utm_source=devtools-fm&utm_campaign=oss">
-          <img src="/vercel.svg" />
-        </FooterLink>
-      </p>
+      <ul className="flex space-x-4 justify-center my-8">
+        {PODCAST_LINKS.map((link) => (
+          <li>
+            <FooterLink href={link.href} title={link.text}>
+              <img
+                src={link.src}
+                className={makeClass(
+                  "h-12 w-12 rounded-xl",
+                  link.src === "/youtube.png" &&
+                    "border border-gray-300 dark:border-none"
+                )}
+              />
+            </FooterLink>
+          </li>
+        ))}
+      </ul>
 
-      <p className="text-sm">
-        Built with <FooterLink href="https://nextjs.org">Next.js</FooterLink>{" "}
+      <p className="text-sm mb-8">
+        Built with <FooterLink href="https://nextjs.org">Next.js</FooterLink>
+        {", "}
+        <FooterLink href="https://github.com/intuit/devtools-ds">
+          devtools-ds
+        </FooterLink>{" "}
         and <FooterLink href="https://tailwindcss.com">tailwindcss</FooterLink>.
         Hosted on <FooterLink href="https://vercel.com">Vercel</FooterLink> and
         the source code is on{" "}
@@ -35,6 +84,12 @@ export const Footer = () => {
           GitHub
         </FooterLink>
         .
+      </p>
+
+      <p className="flex justify-center">
+        <FooterLink href="https://vercel.com?utm_source=devtools-fm&utm_campaign=oss">
+          <img src="/vercel.svg" />
+        </FooterLink>
       </p>
     </footer>
   );
