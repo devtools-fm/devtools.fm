27b126eaa3415f3154cebd88707355d8aab938bc
Andrew Lisowski <lisowski54@gmail.com>
2023-07-22 23:55:11 -0700

    add subscribe link

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 const withEsmCss = require("@design-systems/next-esm-css")(["@devtools-ds/.*"]);
 
-module.exports = withEsmCss({
-  productionBrowserSourceMaps: true,
-});
+module.exports = {
+  ...withEsmCss({
+    productionBrowserSourceMaps: true,
+  }),
+  async redirects() {
+    return [
+      {
+        source: "/subscribe",
+        destination: "podcast://anchor.fm/s/dd6922b4/podcast/rss",
+        permanent: true,
+      },
+    ];
+  },
+};
