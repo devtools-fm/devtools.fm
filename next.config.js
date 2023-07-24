d14196ba49eb5120642977c90e53801b4cb321d4
Andrew Lisowski <lisowski54@gmail.com>
2023-07-23 18:55:07 -0700

    update redirect

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ module.exports = {
     return [
       {
         source: "/subscribe",
-        destination: "podcast://anchor.fm/s/dd6922b4/podcast/rss",
+        destination:
+          "https://podcasts.apple.com/us/podcast/devtools-fm/id1566647758?itsct=podcast_box&itscg=30200&ls=1",
         permanent: true,
       },
     ];
