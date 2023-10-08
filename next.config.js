79b838f0c291b04920c36f964c37d7a7182fee75
Andrew Lisowski <lisowski54@gmail.com>
2023-10-08 16:02:36 -0700

    add redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,20 @@ const withEsmCss = require("@design-systems/next-esm-css")(["@devtools-ds/.*"]);
 
 module.exports = withEsmCss({
   productionBrowserSourceMaps: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        basePath: false,
+        permanent: false,
+        has: [
+          {
+            type: "host",
+            value: "mail.devtools.fm",
+          },
+        ],
+        destination: "https://devtoolsfm.beehiiv.com/subscribe",
+      },
+    ];
+  },
 });
